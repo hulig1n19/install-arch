86205b2bfa8c9f745d1d2fb58c8479e4280156b4
Krzysiek <hulig1n19@gmail.com>
2026-01-17 23:52:31 +0100

    install-arch.sh

--- install-arch.sh
+++ install-arch.sh
@@ -117,7 +117,7 @@ PACMAN_PKGS=(
   plasma-nm plasma-pa bluedevil plasma-workspace-wallpapers plasma-browser-integration
   ark p7zip unrar unzip lrzip lzop zip papirus-icon-theme capitaine-cursors
   kdeplasma-addons kdeconnect sshfs noto-fonts-cjk noto-fonts-extra cantarell-fonts hunspell-pl
-  lact lib32-mesa lib32-vulkan-radeon clinfo kwalletmanager
+  lact lib32-mesa lib32-vulkan-radeon clinfo kwalletmanager chromium
   openrgb steam qbittorrent obs-studio elisa thunderbird putty okular filezilla gsmartcontrol
   teamspeak3 gwenview kdegraphics-thumbnailers ffmpegthumbs reaper mangohud occt btop spectacle timeshift
   kolourpaint gnome-maps gnome-calendar kcalc sweeper vlc vlc-plugins-all scrcpy libreoffice-fresh
@@ -194,7 +194,7 @@ fi
 ###############################################
 
 AUR_PKGS=(
-  heroic-games-launcher opencl-amd xone-dkms google-chrome arch-update
+  heroic-games-launcher opencl-amd xone-dkms arch-update
 )
 
 for pkg in "${AUR_PKGS[@]}"; do
